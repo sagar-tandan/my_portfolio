670eb35a087778b6c8b77388792c63c0e5865a96
Sagar Tandan <076bct072@ioepc.edu.np>
2024-05-02 20:10:36 +0545

    updated darkmode

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -15,7 +15,7 @@ function ThemeProvider({ children }) {
   useEffect(() => {
     localStorage.setItem('darkMode', JSON.stringify(darkMode));
     // document.body.classList.toggle('bg-[#fffff]', !darkMode); // Light mode background color
-    document.body.classList.toggle('bg-stone-900', darkMode); 
+    document.body.classList.toggle('bg-#272d35', darkMode); 
  
 
   }, [darkMode]);
